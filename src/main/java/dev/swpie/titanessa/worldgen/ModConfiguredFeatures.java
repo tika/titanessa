86e7fe6cbf04b75fb3fccc9a824e45b4e8a082a8
tika <48658947+tika@users.noreply.github.com>
2023-08-07 00:29:14 +0100

    feat: datagen works if i don't use ballcluster

--- src/main/java/dev/swpie/titanessa/worldgen/ModConfiguredFeatures.java
+++ src/main/java/dev/swpie/titanessa/worldgen/ModConfiguredFeatures.java
@@ -2,7 +2,6 @@ package dev.swpie.titanessa.worldgen;
 
 import dev.swpie.titanessa.Titanessa;
 import dev.swpie.titanessa.item.ModBlocks;
-import dev.swpie.titanessa.worldgen.feature.BallCluster;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
@@ -12,8 +11,8 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.ProbabilityFeatureConfiguration;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
@@ -35,7 +34,7 @@ public class ModConfiguredFeatures {
 
 //        register(context, COSMIC_VEIN_KEY, Feature.ORE, new OreConfiguration(overworldStarstone, 9));
 
-        register(context, COSMIC_VEIN_KEY, new BallCluster(), new ProbabilityFeatureConfiguration(1.0F));
+        register(context, COSMIC_VEIN_KEY, Feature.VINES, new NoneFeatureConfiguration());
     }
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
