ed18669492c95cbd686f63b80c25335949fb1de5
tika <48658947+tika@users.noreply.github.com>
2023-08-06 21:16:54 +0100

    feat: generate starstone underground CORRECTLY (using features)

---
+++ src/main/java/dev/swpie/titanessa/datagen/ModWorldGenProvider.java
@@ -0,0 +1,23 @@
+package dev.swpie.titanessa.datagen;
+
+import dev.swpie.titanessa.Titanessa;
+import dev.swpie.titanessa.world.feature.ModConfiguredFeatures;
+import dev.swpie.titanessa.world.feature.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
+
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(Titanessa.MOD_ID));
+    }
+}
---
+++ src/main/java/dev/swpie/titanessa/world/feature/ModConfiguredFeatures.java
@@ -0,0 +1,45 @@
+package dev.swpie.titanessa.world.feature;
+
+import dev.swpie.titanessa.Titanessa;
+import dev.swpie.titanessa.item.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+
+import java.util.List;
+
+public class ModConfiguredFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> STARSTONE_KEY = registerKey("starstone");
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(Titanessa.MOD_ID, name));
+    }
+
+    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherrackReplaceables = new BlockMatchTest(Blocks.NETHERRACK);
+        RuleTest endstoneReplaceables = new BlockMatchTest(Blocks.END_STONE);
+
+        List<OreConfiguration.TargetBlockState> overworldStarstone = List.of(OreConfiguration.target(stoneReplaceables,
+                        ModBlocks.STARSTONE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.STARSTONE.get().defaultBlockState()));
+
+        register(context, STARSTONE_KEY, Feature.ORE, new OreConfiguration(overworldStarstone, 9));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(
+            BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
