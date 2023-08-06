6d12c4396bc7f5b75d7875e92a1d8c1f8b4459ab
tika <48658947+tika@users.noreply.github.com>
2023-08-06 14:54:25 +0100

    feat: add creative mode tab

---
+++ src/main/java/dev/swpie/titanessa/item/ModCreativeModeTabs.java
@@ -0,0 +1,30 @@
+package dev.swpie.titanessa.item;
+
+import dev.swpie.titanessa.Titanessa;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Titanessa.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TITANESSA_TAB = CREATIVE_MODE_TABS.register("titanessa_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.CELESTIUM_INGOT.get()))
+                    .title(Component.translatable("creativetab.titanessa_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.CELESTIUM_INGOT.get());
+                        pOutput.accept(ModItems.CELESTIUM_CHUNK.get());
+                    })
+                    .build()
+    );
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
